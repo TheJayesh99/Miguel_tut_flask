caa46aecc33e14b795b6541d99d63ec79c450628
Jayesh Mali <jayeshm.officail@gmail.com>
2021-11-27 18:42:28 +0530

    Added : Templets to display pages

--- main_app/routes.py
+++ main_app/routes.py
@@ -1,6 +1,18 @@
 from main_app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Jayesh'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',params=locals())
