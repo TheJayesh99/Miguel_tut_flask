bca9a32794940fc00c73f08a1ed6062102194481
Jayesh Mali <jayesh.mali@esthenos.com>
2021-12-04 17:09:16 +0530

    Added : user login

--- main_app/models.py
+++ main_app/models.py
@@ -2,8 +2,11 @@ from datetime import datetime
 
 from main_app import db
 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from main_app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique= True, index=True)    
@@ -14,6 +17,12 @@ class User(db.Model):
     def __repr__(self) -> str:
         return f'<User {self.username}>'
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Posts(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -23,3 +32,9 @@ class Posts(db.Model):
 
     def __repr__(self) -> str:
         return f'Post {self.body}'
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
