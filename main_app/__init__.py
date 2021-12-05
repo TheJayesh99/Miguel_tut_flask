691b23a1a4a94ae79ee9e1d12cc929b5bd035260
Jayesh Mali <68943760+TheJayesh99@users.noreply.github.com>
2021-12-05 12:03:23 +0530

    Added : error handling in models

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,9 +1,10 @@
+import logging
+
 from config import Config
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +13,25 @@ login.login_view = "login"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from main_app import routes, models
+import os
+from logging.handlers import RotatingFileHandler
+
+from main_app import errors, models, routes
+
+if not app.debug:
+
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler(
+        "logs/microblog.log", maxBytes=10240, backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Microblog startup")
