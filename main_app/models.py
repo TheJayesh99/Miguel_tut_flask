fefd078a54601fd1ffaf203e73c7c3d19e62bdb2
Jayesh Mali <68943760+TheJayesh99@users.noreply.github.com>
2021-12-12 16:51:41 +0530

    Added : forgot and reset password

--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,11 @@
 from datetime import datetime
-
-from main_app import db
-
-
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from main_app import login
-
 from hashlib import md5
 
+import jwt
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from main_app import app, db, login
 
 followers = db.Table(
     "followers",
@@ -68,6 +65,14 @@ class User(UserMixin, db.Model):
         own = Posts.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Posts.timestamp.desc())
 
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 class Posts(db.Model):
 
