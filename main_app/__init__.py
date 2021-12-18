ead8d4b343649bac1372e9084a5a7eb6ff938cc6
Jayesh Mali <68943760+TheJayesh99@users.noreply.github.com>
2021-12-18 17:08:59 +0530

    Added : datatime feture to display time nicely

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 import os
 from logging.handlers import RotatingFileHandler
